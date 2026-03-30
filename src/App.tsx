cd5a6ad0279809498232b36fd6e90e34887c9995
Shivam Mishra <mshivam2062@gmail.com>
2026-03-30 17:55:20 +0530

    Brainly With Frontent-TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import { PlusIcon } from './Icons/PlusIcons.tsx';
 function App() {
   return (
     <>
-      <Button startIcon={<Pnt" onClick={() => {}} />
+      <Button startIcon={<PlusIcon/>}  variant="primary" size="md" text="Share" onClick={() => {}} />
+      <Button variant="secondary" size="md" text="Add Content" onClick={() => {}} />
 
     </>
   );
