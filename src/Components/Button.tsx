ea57c754cbfea0bedb6ac02b38b9c45b94f25e0a
Shivam Mishra <mshivam2062@gmail.com>
2026-03-31 11:16:15 +0530

    Brainly With Frontent-TypeScript

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,38 +1,39 @@
 import { ReactElement } from "react";
 
 interface ButtonInterface {
-  title: string;
-  size: "lg" | "md" | "sm";
-  startIcon?: ReactElement;
-  endIcon?: ReactElement;
-  variant: "primary" | "secondary";
+    title : string,
+    size : "lg" | "md" | "sm";
+    startIcon?: ReactElement;
+    endIcon?: ReactElement;
+    variant: "primary" | "secondary";
 }
 
-const sizeStyles = {
-  lg: "px-8 py-4 text-xl rounded-xl",
-  md: "px-4 py-2 text-md rounded-md",
-  sm: "px-2 py-1 text-sm rounded-sm",
-};
 
-const variantStyle = {
-  primary: "bg-purple-600 text-white hover:bg-purple-700",
-  secondary: "bg-purple-100 text-purple-600 hover:bg-purple-200",
-};
 
-export function Button({
-  title,
-  size,
-  startIcon,
-  endIcon,
-  variant,
-}: ButtonInterface) {
+const sizeStyle  = {
+    "lg" : "px-8 py-4 text-xl rounded-xl ",
+    "md" : "px-4 py-2 text-md rounded-md",
+    "sm" : "px-2 py-1 text-sm rounded-sm",
+
+
+}
+
+const variantStyle  = {
+    "primary" : "bg-purple-600 text-white",
+    "secondary" : " bg-purple-400 text-purple-600 "
+}
+
+export function Button(props: ButtonInterface) {
+
   return (
-    <button
-      className={`${sizeStyles[size]} ${variantStyle[variant]} flex items-center gap-2`}
-    >
-      {startIcon}
-      <span>{title}</span>
-      {endIcon}
+    <button className={sizeStyles[props.size] + " " + variantStyle[props.variant] }>
+      <div className="flex">
+        {props.startIcon}
+        <div className="pl-2 pr-2">
+          {props.title}
+        </div>
+        {props.endIcon}
+      </div>
     </button>
-  );
+  )
 }
